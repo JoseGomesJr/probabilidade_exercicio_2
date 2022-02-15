89bbe171f987a0feae7be1825aea7667786a769e
edubarr <eduaraujobarros@gmail.com>
2022-02-15 20:47:33 -0300

    add Q4

--- Q4.r
+++ Q4.r
@@ -1,6 +1,43 @@
+library(readxl)
+library(corrplot)
 
-nota <- subset(dados , NO_MUNICIPIO_PROVA == municipios[1])
-notas_media <- mean(nota$NOTA_ENEN)
+planilha <- read_excel("ENEM_AL_EXCEL_AJUS_OKSNZ.xlsx")
+
+dados <- as.data.frame(planilha)
+
+notas_medias <- as.data.frame(aggregate(cbind(
+    dados$NU_NOTA_CN, dados$NU_NOTA_CH,
+    dados$NU_NOTA_LC, dados$NU_NOTA_MT, dados$NU_NOTA_REDACAO
+),
+list(dados$NO_MUNICIPIO_PROVA),
+FUN = mean
+))
+
+rownames(notas_medias) <- notas_medias[, 1]
+notas_medias <- notas_medias[, -1]
+
+colnames(notas_medias) <- c("CN", "CH", "LC", "MT", "RED")
+
+matriz_corr <- cor(notas_medias)
+
+regressao <- lm(
+    formula = notas_medias$LC ~ notas_medias$CN,
+    data = notas_medias
+)
+
+print(notas_medias)
+
+summary(notas_medias)
+
+print(matriz_corr)
+
+corrplot(matriz_corr, method = "pie")
+
+summary(regressao)
 
 
-View(municipios.notas)
+plot(notas_medias$LC ~ notas_medias$CN,
+    data = notas_medias,
+    main = "Correlacao - Linguagens e Codigos e Ciencias da Natureza"
+)
+abline(regressao)
