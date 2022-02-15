66616f50a3f9beb80b245d6f950c0c5b7f00b63e
edubarr <eduaraujobarros@gmail.com>
2022-02-15 18:43:59 -0300

    change path to relative

--- Q1.r
+++ Q1.r
@@ -1,8 +1,8 @@
 library(readxl)
-planilha <- read_excel("c:\\Users\\josej\\OneDrive\\Documentos\\GitHub\\probabilidade_exercicio_2\\ENEM_AL_EXCEL_AJUS_OKSNZ.xlsx", sheet=1, col_names=TRUE)
+planilha <- read_excel("ENEM_AL_EXCEL_AJUS_OKSNZ.xlsx", col_names = TRUE)
 dados <- as.data.frame(planilha)
 nota <- dados$NOTA_ENEN
 
-t.test(nota, conf=0.95)
+t.test(nota, conf = 0.95)
 
-t.test(nota, conf=0.99)
+t.test(nota, conf = 0.99)
